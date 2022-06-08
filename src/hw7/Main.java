0ee390649d879f41ae8d1d96e961df4601bd937a
IrinaDash <irina.lev@list.ru>
2022-06-08 23:49:48 +0300

    add Manager2

--- src/hw7/Main.java
+++ src/hw7/Main.java
@@ -1,2 +1,13 @@
-package hw7;public class Main {
+package hw7;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Worker worker = new Worker(1000, "David");
+        Manager manager = new Manager(1000, "Kristen", 80);
+        Manager manager2 = new Manager(1000, "Dia", 80);
+
+        System.out.println(manager.getSalary());
+    }
 }
