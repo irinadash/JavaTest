4ce1a8987235cf1b620de2a7e74a4c92c3df1856
IrinaDash <irina.lev@list.ru>
2022-06-09 22:43:34 +0300

    add Managers

--- src/hw7/Main.java
+++ src/hw7/Main.java
@@ -6,8 +6,9 @@ public class Main {
 
         Worker worker = new Worker(1000, "David");
         Manager manager = new Manager(1000, "Kristen", 80);
-        Manager manager2 = new Manager(1000, "Dia", 80);
-
+        Manager manager2 = new Manager(3000, "Dia", 80);
+        Manager manager3 = new Manager(5000, "Rob", 80);
+        Manager manager4 = new Manager(10000, "Carol", 80);
         System.out.println(manager.getSalary());
     }
 }
