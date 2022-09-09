8bb4c6b39c918bebb2338b662bc62e17923e5b8f
IrinaDash <irina.lev@list.ru>
2022-09-09 23:14:25 +0300

    add loop

--- src/Sep.java
+++ src/Sep.java
@@ -1,7 +1,8 @@
 public class Sep {
     public static void main(String[] args) {
-        int s1 = 5;
-        System.out.println(s1++);
-        System.out.println(++s1);
+        for(int i = 0; i<10; i++){
+            System.out.println(i);
+        };
+
     }
 }
