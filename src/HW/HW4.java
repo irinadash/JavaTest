f3e0f6fed7605f3081f3578613d3e4b11f4ec293
IrinaDash <irina.lev@list.ru>
2022-09-24 17:04:00 +0300

    task4

--- src/HW/HW4.java
+++ src/HW/HW4.java
@@ -1,4 +1,22 @@
 package HW;
 
+import java.sql.SQLOutput;
+
 public class HW4 {
+   // printNumber();
+//    int a = 11;
+//    System.out.println(a)
+//
+//
+//    task();
+//    int age1 = 160;
+//    if(age <= 150 && age >= 0){
+//        if(age1 >= 18){
+//            System.out.println("голосовать можно");
+//        } else {
+//            System.out.println("голосовать нельзя");
+//        }
+//    } else {
+//        System.out.println("please enter correct number");
+//    }
 }
